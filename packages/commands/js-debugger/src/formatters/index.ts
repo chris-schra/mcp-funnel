2e65cf65ce06de1b6c4272c68d80931e835a5834
Chris Schramm <cschramm@cyberport.de>
2025-10-02 11:35:08 +0200

    remove js-debugger

--- packages/commands/js-debugger/src/formatters/index.ts
+++
@@ -1,6 +0,0 @@
-export { DebugResponseFormatter } from './debug-response-formatter.js';
-export { BaseResponseFormatter } from './base-formatter.js';
-export { SessionFormatter } from './session-formatter.js';
-export { StackFormatter } from './stack-formatter.js';
-export { VariableFormatter } from './variable-formatter.js';
-export type { IResponseFormatter } from '../types/index.js';
--- packages/commands/js-debugger/test/fixtures/node/auto-exit.js
+++
@@ -1,4 +0,0 @@
-/* eslint-env node */
-
-console.log('Script is running');
-setTimeout(() => process.exit(0), 500);
