ba2c6b2c1b66e7e80fb05f4aede9bf4f25468722
Chris Schramm <cschramm@cyberport.de>
2025-10-08 13:32:27 +0200

    vitest: redirect logs to pino

---
+++ vitest.setup.ts
@@ -0,0 +1,9 @@
+import pino from 'pino';
+
+const testLogger = pino({ level: process.env.LOG_LEVEL || 'warn' });
+(['debug', 'info', 'warn', 'error', 'log'] as const).forEach((name) => {
+  // eslint-disable-next-line no-console
+  console[name] = (...args: unknown[]) => {
+    testLogger[name === 'log' ? 'debug' : name]?.(args);
+  };
+});
