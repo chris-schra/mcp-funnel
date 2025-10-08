aca4ef1c58f1dc26c181d2cd90fb7654a9efbe06
Chris Schramm <cschramm@cyberport.de>
2025-10-08 12:48:32 +0200

    chore: remove duplicate pattern for configs in vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -23,7 +23,17 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['node_modules/', '**/test/*', '*.config.ts', 'src/cli.ts', "**/*.test.ts", "**/build.ts", "**/build/*", "**/dist/*", "**/.react-router/*", "**/*.config.ts"],
+      exclude: [
+        'node_modules/',
+        '**/test/*',
+        'src/cli.ts',
+        '**/*.test.ts',
+        '**/build.ts',
+        '**/build/*',
+        '**/dist/*',
+        '**/.react-router/*',
+        '**/*.config.ts',
+      ],
     },
     testTimeout: 10000,
     hookTimeout: 10000,
